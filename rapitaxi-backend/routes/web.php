fd04f3dfa1dcfc976b940c2ede35bbc91b8dfd71
Ronald Tubay <ronaldtubay27@gmail.com>
2026-07-02 00:22:00 -0500

    ruta

--- rapitaxi-backend/routes/web.php
+++ rapitaxi-backend/routes/web.php
@@ -2,11 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash; 
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/instalar-bd', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return '¡Migraciones ejecutadas con éxito en Render!';
+
+Route::get('/crear-admin', function () {
+    if (User::where('email', 'admin@rapitaxi.com')->exists()) {
+        return 'El usuario administrador ya existe en la base de datos.';
+    }
+
+    User::create([
+        'name' => 'Administrador Rapitaxi',
+        'email' => 'admin@rapitaxi.com', 
+        'password' => Hash::make('12345678') 
+    ]);
+
+    return '¡Usuario administrador creado con éxito! Ya puedes iniciar sesión.';
 });
