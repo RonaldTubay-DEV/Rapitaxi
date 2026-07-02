85c523d3dec5a30a6d1be4ed28a659069c128d1f
Ronald Tubay <ronaldtubay27@gmail.com>
2026-07-02 00:01:18 -0500

    migracion_render

--- rapitaxi-backend/routes/web.php
+++ rapitaxi-backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/instalar-bd', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return '¡Migraciones ejecutadas con éxito en Render!';
 });
